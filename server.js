64d9ee245c228775571266610556e4d4d6475d80
du7346317-hash <du7346317@gmail.com>
2026-08-22 01:50:35 -0300

    server.js

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Diz para o servidor entregar os arquivos da mesma pasta
-app.use(express.static(__dirname));
+// Avisamos o servidor que as coisas do frontend (HTML, CSS) estão na pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Quando alguém acessar o site, entrega o index.html
+// Quando acessar o site, ele busca o index.html lá dentro
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Porta dinâmica para rodar perfeitamente no Render
+// Porta dinâmica pro Render
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`Servidor rodando liso na porta ${PORT}!`);
